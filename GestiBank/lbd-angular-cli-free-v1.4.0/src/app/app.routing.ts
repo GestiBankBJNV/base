cf3c9ede6b664ca42590a2e3ab79cc8e909cef4d
vjarzembowski <35570285+vjarzembowski@users.noreply.github.com>
2018-01-19 16:25:12 +0100

    MAJ 19/01
    Commentaires, nettoyage, correction de navbar, ajout du composant notifications

--- GestiBank/lbd-angular-cli-free-v1.4.0/src/app/app.routing.ts
+++ GestiBank/lbd-angular-cli-free-v1.4.0/src/app/app.routing.ts
@@ -3,14 +3,15 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
+//IMPORTS TEMPLATE
 import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { TablesComponent } from './tables/tables.component';
-import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { TablesComponent } from './tables/tables.component';
+import { TypographyComponent } from './typography/typography.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
+import { UserComponent } from './user/user.component';
 
 //IMPORTS PUBLIC
 import { ConnexionComponent } from './connexion/connexion.component';
@@ -35,6 +36,7 @@ import { ClientProfilComponent } from './client-profil/client-profil.component';
 
 const routes: Routes =[
     { path: 'admin_accueil',         component: AdminAccueilComponent },        //ROUTES ADMIN
+    { path: 'admin_devises',        component: PublicDevisesComponent },
     { path: 'admin_fiche_conseiller',component: AdminFicheConseillerComponent },      
     { path: 'client_accueil',        component: ClientAccueilComponent },       //ROUTES CLIENT
     { path: 'client_comptes',        component: ClientComptesComponent },
@@ -42,8 +44,10 @@ const routes: Routes =[
     { path: 'client_notifications',  component: ClientNotificationsComponent },
     { path: 'client_profil',         component: ClientProfilComponent },
     { path: 'conseiller_accueil',    component: ConseillerAccueilComponent },   //ROUTES CONSEILLER
+    { path: 'conseiller_devises',    component: PublicDevisesComponent },
     { path: 'public_accueil',        component: EspacePublicComponent },        //ROUTES PUBLIC
     { path: 'public_connexion',      component: ConnexionComponent },
+    { path: 'public_devises',        component: PublicDevisesComponent },
     { path: 'dashboard',      component: HomeComponent },                        //ROUTES TEMPLATE
     { path: 'user',           component: UserComponent },
     { path: 'table',          component: TablesComponent },
