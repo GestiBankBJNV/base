6092b0d918f5d11c05121101c69dbbf783c08fbc
nassimbenredouane <benredouane.nassim@gmail.com>
2018-01-19 16:10:25 +0100

    ajout des contrôles sur le login

--- GestiBank/lbd-angular-cli-free-v1.4.0/src/app/connexion/connexion.component.ts
+++ GestiBank/lbd-angular-cli-free-v1.4.0/src/app/connexion/connexion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { ROUTES, ROUTES_PUBLIC } from './../sidebar/sidebar.component';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
@@ -10,9 +10,25 @@ import { FormGroup, FormControl} from '@angular/forms';
 })
 export class ConnexionComponent implements OnInit {
 
-  constructor() { }
+	formGroupLogin: FormGroup;
+
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+  	// this.formGroupLogin = new FormGroup({
+  	// 	emailLogin: new FormControl(),
+  	// 	passwordLogin: new FormControl()
+  	//});
+
+  	this.formGroupLogin = this.fb.group({
+  		emailLogin: ['', Validators.required],
+  		passwordLogin: ['', Validators.required]
+  	})
+
+  	
+}
+  verifLogin(){
+
   }
 
 }
