2d4548815d62819b10205371999796f72fee56a0
vjarzembowski <35570285+vjarzembowski@users.noreply.github.com>
2018-01-31 11:08:39 +0100

    trad en place
    enfin, le début

--- GestiBank/lbd-angular-cli-free-v1.4.0/src/app/client-accueil/client-accueil.component.ts
+++ GestiBank/lbd-angular-cli-free-v1.4.0/src/app/client-accueil/client-accueil.component.ts
@@ -3,6 +3,7 @@ import { Notification } from '../classes/notification';
 import { Client } from '../classes/client';
 import { Compte } from '../classes/compte';
 import { CLIENT } from '../classes/FAKES';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-client-accueil',
@@ -11,7 +12,13 @@ import { CLIENT } from '../classes/FAKES';
 })
 export class ClientAccueilComponent implements OnInit {
 	client : Client = CLIENT;
-  constructor() { }
+  
+  constructor(translate : TranslateService) {
+     // this language will be used as a fallback when a translation isn't found in the current language
+     translate.setDefaultLang('en');
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+     translate.use('en');
+  }
 
   ngOnInit() {
   }
