1cdfcdac0070f67ee98ed25359bdc90ad7a7b898
vjarzembowski <35570285+vjarzembowski@users.noreply.github.com>
2018-02-14 15:07:58 +0100

    service compte²

--- GestiBank/lbd-angular-cli-free-v1.4.0/src/app/compte-service.ts
+++ GestiBank/lbd-angular-cli-free-v1.4.0/src/app/compte-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Compte } from './data-model';
+import { Compte, Operation } from './data-model';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -7,13 +7,25 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CompteService{
-	private apiUrl = "http://localhost:8080/Brijanavi";
+	private apiUrl = "http://localhost:8080/GestiBankBrijanavi/";
 
 	constructor(private http : Http) {}
 
 	//Renvoie une liste de taux de conversion. base = EUR par défaut
-	getConversionRates(base : string) : Observable<Compte>{
-		return this.http.get(this.apiUrl + "?base=" + base)
+	getComptesByClient(clientID : string) : Observable<Compte[]>{
+		return this.http.get(this.apiUrl + "comptes/" + clientID)
+		.map((res : Response) => res.json())
+		.catch((error : any) => Observable.throw(error.json().error || 'Error'));
+	}
+
+	getCompteByID(compteID : number) : Observable<Compte> {
+		return this.http.get(this.apiUrl + "compte/" + compteID)
+		.map((res : Response) => res.json())
+		.catch((error : any) => Observable.throw(error.json().error || 'Error'));
+	}
+
+	getCompteOperations(compteID : number) : Observable<Operation>{
+		return this.http.get(this.apiUrl + "operations/" + compteID)
 		.map((res : Response) => res.json())
 		.catch((error : any) => Observable.throw(error.json().error || 'Error'));
 	}
