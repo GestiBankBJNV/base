0cc4fddefb6e550ea533f34e25444bdd6cc698f8
JeanneGKJava <35568941+JeanneGKJava@users.noreply.github.com>
2018-02-19 16:59:03 +0100

    nettoyage

--- GestiBankBrijanavi/src/com/gk/gestibank/dao/OperationDao.java
+++
@@ -1,9 +0,0 @@
-package com.gk.gestibank.dao;
-
-import java.util.List;
-
-import com.gk.gestibank.model.Operation;
-
-public interface OperationDao {
-	List<Operation> getByCompte(int compteId);
-}
