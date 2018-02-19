8a99e26b1000ecae92dc629949c78e53d65ef2e7
JeanneGKJava <35568941+JeanneGKJava@users.noreply.github.com>
2018-02-19 17:01:56 +0100

    Récupération du projet fonctionnel

---
+++ GestiBankBrijanavi/src/main/java/com/gk/gestibank/web/WSCompte.java
@@ -0,0 +1,66 @@
+package com.gk.gestibank.web;
+
+import java.util.List;
+
+
+
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gk.gestibank.model.Compte;
+import com.gk.gestibank.model.CompteEpargne;
+import com.gk.gestibank.model.Notification;
+import com.gk.gestibank.model.Operation;
+import com.gk.gestibank.services.impl.CompteService;
+
+
+@RestController
+@Path("/comptes")
+public class WSCompte {
+	
+	//Service
+	@Autowired
+	CompteService compteService;
+	
+	//Constructeur
+	public WSCompte(){}
+
+	/**
+	 * Récupérer tous les comptes d'un client
+	 * @param clientId : ID du client
+	 * @return List<Notification>
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/client/{clientId}")
+	public List<Compte> getComptesByClient(@PathParam("clientId") int clientId){
+		dbgLog("Get by client" + clientId);
+		return compteService.getByClient(clientId);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/operations/{compteId}")
+	public List<Operation> getOperationsByCompte(@PathParam("compteId") int compteId){
+		dbgLog("Get operations by compte" + compteId);
+		return compteService.getOperationsByCompte(compteId);
+	}
+	
+	
+	
+	//DEBUG (juste pour vérifier qu'on passe bien par les fonctions)
+	private void dbgLog(String log){
+		System.out.println("SERVICE COMPTE - " + log);
+	}
+}
